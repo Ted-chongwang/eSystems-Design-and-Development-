a312406edba3fd8a707e882440c32df72e6e4e83
wch8812 <53140860+wch8812@users.noreply.github.com>
2020-05-05 21:15:59 +1200

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
     <div class="paragraphmargin">
       <p class="pleft">    Name:Chong Wang</p>
       <p class="pleft">    Student ID:19040821</p>
+      <p class="pleft">    Email:crq2021@autuni.ac.nz</p>
     </div>
       <h3>Abstract</h3>
       <p class="paragraphmargin">oject management. Several agile software planning tools were developed; they grow in number and complexity. Our analysis of 30 of the most known agile planning tools shows that while the majority of them support the key agile planning activities, they still have limitations. In this paper, we presented three limitations to the current agile planning tools and we developed three features to overcome them. A questionnaire is used to evaluate the identified limitations and the proposed features. The evaluation shows that the identified limitations are sound and exist in the real software projects. In addition, most of the participants agree about how to implement the first and second proposed features (providing task effort notification and improving user story prioritization). The evaluation of the third feature (adding new user story/re-prioritize user story by customer) shows that the simple implementation provided for this feature is not sufficient and, hence, there is a need for a more thorough study to best involve customers through agile planning tools. This research contributes in developing advanced agile planning tools supporting the key agile planning activities.
