0b5d89b02cb78e7b8083216c571b32c195ce596b
wch8812 <53140860+wch8812@users.noreply.github.com>
2020-05-05 21:46:13 +1200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>NoSQL Development: MongoDB</title>
     <style>
-       .paragraphmargin {margin-bottom:6%;}
+       .sectionmargin {margin-bottom:6%;}
+       .paragraphmargin {margin-bottom:3%;}
        .mainmargin   {maigin-bottom:5%;margin-left:10%;margin-right:10%;margin-top:5%;}
        p {text-align:justify;}
        p.pleft {text-align:left;}
@@ -18,7 +19,7 @@
   </head>
   
   <body class="mainmargin">
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h2>NoSQL Development: MongoDB</h1>
       </div>
       <div class="paragraphmargin">
@@ -27,13 +28,13 @@
         <p class="pleft">    Email: crq2021@autuni.ac.nz</p>
       </div>
 
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>Abstract</h3>
         <p class="paragraphmargin">MongoDB is a high-volume distributed NoSQL database that is able to effectively and efficiently store and process big data. This research paper will explore some MongoDB core principle.
         </p>
       </div>
 
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>I. INTRODUCTION</h3>
         <p class="paragraphmargin">NoSQL first appeared in 1998 and was developed by Carlo Strozzi as a lightweight, open-source relational database that does not provide SQL functionality [1]. The shortcomings that relational databases cannot overcome were becoming more and more obvious after the rise of the Web 2.0 [2]. With the needs of the NoSQL market, MongoDB was first released in 2009 and has since been updated to the fourth major releases. MongoDB service can be run in the cloud base as MongoDB Atlas that available on AWS, Azure, and GCP cloud platforms or installed in local servers like Ubuntu. MongoDB can be easily operated by using MongoDB statements. However, developers need to understand the working mechanisms to better develop the database.
         </p>
@@ -41,7 +42,7 @@
         </p>
       </div>
 
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>II. DATA MODEL</h3>
         <p class="paragraphmargin">The difference between data models is the method they store and access data. MongoDB was developed based on the document store modal, and it stores the data as key-value pairs in Binary JSON (BSON) format document. This feature makes MongoDB high performance and horizontal scalability [3]. In the BSON files, the concept of table, row and columns in a relational database is transformed into a collection, document and field.
         </p>
@@ -49,37 +50,37 @@
         </p>
       </div>
 
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>III. TECHNIQUE</h3>
         <p class="paragraphmargin">
 Each database is designed to work in a different domain to meet the functional and non-functional requirements [6]. Some techniques that are used in MongoDB to better adept big data storage.
         </p>
       </div>
     
-      <div class="paragraphmargin">
-        <h4><A. Sharding/h4>
+      <div class="sectionmargin">
+        <h5>A. Sharding</h5>
         <p class="paragraphmargin">High data volume and throughput of database applications will put a lot of pressure on the performance of a single machine. Using a data model with highly distributed storage catachrestic, MongoDB divides the data and stores them into several nodes. The database developer needs to set the configurations, and the database will automatically do the Sharding work. For client developers, MongoDB Sharding is transparent, and applications only need to operate on the data.
         </p>
       </div>
     
-      <div class="paragraphmargin">
-        <h4>B. Replication</h4>
+      <div class="sectionmargin">
+        <h5>B. Replication</h5>
         <p class="paragraphmargin">of improving fault tolerance for big data storage [7]. The architecture of MongoDB replication is that a Primary node connects with several Secondary nodes. The data will be directly updated into the Primary node and the operation will be recorded into oplog in the Primary node. Then Secondary nodes will read the oplog and update the data that what Primary node did. So that, the data on the Primary and Secondary nodes ensure the final consistency. When the Primary node fails, one Secondary node will be automatically selected as the Primary node, so that the database can run continuously.
         </p>
       </div>
           
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>IV. SECURITY</h3>
         <p class="paragraphmargin">Security is very important for any program, and MongoDB provides a variety of mechanisms to ensure the security of the data. MongoDB provides an access control mechanism that restricts only clients and servers with valid credentials can join the system. Like other databases, MongoDB supports multi-user permissions, limited the permissions of different users to access and operate data. MongoDB uses TLS / SSL to support the secure communication between the client and the database or Inside the database cluster. At the same time, we can encrypt the documents to avoid bypassing the security mechanism and obtaining the data directly.
         </p>
       </div>
  
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>V. CONCLUSION</h3>
         <p class="paragraphmargin">MongoDB is a popular NoSQL database used in various field. Due to the differences with traditional relational databases and other types of NoSQL databases and the continuous updating of MongoDB, developers many get confused about why chose MongoDB, what MongoDB can do and how MongoDB works. This research paper illustrates the data model and the main techniques that MongoDB adopted. Developers can explorer those areas to better implement solutions on MongoDB.
         </p>
         
-      <div class="paragraphmargin">
+      <div class="sectionmargin">
         <h3>REFERENCES</h3>
         <p>[1] C. Strozzi, "NoSQL: A relational database management system," Lainattu, vol. 5, p. 2014, 1998.
         </p>
